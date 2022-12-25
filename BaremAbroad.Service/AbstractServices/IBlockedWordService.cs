d33ef812978bd3abc49b5d96c8db2f4c0321751d
Sanem Yıldız Kavukoğlu <sanemkavukoglu420@gmail.com>
2022-12-25 23:22:26 +0300

    services

---
+++ BaremAbroad.Service/AbstractServices/IBlockedWordService.cs
@@ -0,0 +1,18 @@
+﻿using BaremAbroad.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaremAbroad.Core.Services
+{
+    public interface IBlockedWordService
+    {
+        Task<BlockedWord> GetBlockedWordByIdAsync(int Id);
+        Task<BlockedWord> RemoveBlockedWordByIdAsync(int Id);
+        Task<BlockedWord> UpdateBlockedWordByIdAsync(BlockedWord blockedWord);
+        Task<BlockedWord> AddBlockedWordAsync(BlockedWord blockedWord);
+        Task<List<BlockedWord>> GetAllBlockedWordsAsync();
+    }
+}
---
+++ BaremAbroad.Service/AbstractServices/IBlogArticleService.cs
@@ -0,0 +1,18 @@
+﻿using BaremAbroad.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaremAbroad.Core.Services
+{
+    public interface IBlogArticleService
+    {
+        Task<BlogArticle> GetBlogArticleByIdAsync(int Id);
+        Task<BlogArticle> RemoveBlogArticleByIdAsync(int Id);
+        Task<BlogArticle> UpdateBlogArticleByIdAsync(BlogArticle blogArticle);
+        Task<BlogArticle> AddBlogArticleAsync(BlogArticle blogArticle);
+        Task<List<BlogArticle>> GetAllBlogArticlesAsync();
+    }
+}
